cb6d508c19e13bef5238a228e68286479e32eac7
Jesper Kjærgaard <jezper@hotmail.dk>
2023-02-26 21:05:49 +0100

    push

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,9 +10,9 @@
     </div>
 
     <div v-if="isLoggedIn" class="userAccount">
-        <p>Username</p>
+        <p>{{username}}</p>
         <button>Likes</button>
-        <button>Logout</button>
+        <button @click="Logout">Logout</button>
     </div>  
 </div>
 
@@ -27,7 +27,7 @@
         <label for="">Password</label>
         <input type="text">
         
-        <button>Login</button>
+        <button @click="Login">Login</button>
     </div>
     
     <div v-if="showSignIn" id="login">
@@ -58,6 +58,12 @@ export default {
             showLogin: false,
             showSignIn: false,
             showLoginHolder: false,
+
+
+            email: "admin@hotmail.dk",
+            username: "Admin",
+            password: "1234",
+
         }
     },
     methods: {
@@ -81,6 +87,15 @@ export default {
         Exit() {
             this.showLoginHolder = false
         },
+        Login() {
+            this.isLoggedIn = true
+            this.Exit()
+        },
+        Logout() {
+            this.isLoggedIn = false
+            this.Exit()
+        },
+
     }
 }
 </script>
@@ -136,7 +151,7 @@ export default {
     margin-left: 20%;
 }
 
-#main-header #backgroundCheck {
+#backgroundCheck {
     position: absolute; /*position: fixed;*/
     top: 0;
     left: 0;
@@ -207,4 +222,121 @@ button:focus {
 button:active {
     transform: scale(0.99);
 }
+
+
+@media (pointer:none), (pointer:coarse) {
+    #showSignOrLog {
+    z-index: 1;
+    position: absolute;
+    top: 20%;
+    bottom: 40%;
+    left: 10%;
+    right: 10%;
+    border-radius: 5%;
+    background-color: #78a8e2;
+    }
+
+    #showSignOrLog #login{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        position: absolute;
+        top: 0%;
+        bottom: 20%;
+        left: 15%;
+        right: 35%;
+    }
+
+    #login input{
+        background-color: #fff;
+        height: 10%;
+        padding: 5%;
+        font-family: Arial, sans-serif;
+        font-size: 1rem;
+    }
+
+    #login label{
+        padding-top: 5%;
+    }
+
+    #main-header #logo {
+    margin-left: 10%;
+    }
+
+    #showSignOrLog #exit{
+        float: right;
+        margin-right: 20px;
+        margin-top: 20px;
+    }
+
+    #backgroundCheck {
+        position: absolute; /*position: fixed;*/
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.5);
+    }
+
+    .userAccount{
+        margin-right: 0%;
+        align-self: center;
+    }
+    
+    .userAccount p{
+        margin-left: 15%;
+    }
+
+    .userAccount button{
+        margin: 1px;
+    }
+
+    .userAccount p{
+        display: inline-block;
+        border: none;
+        border-radius: 10px;
+        padding: 1rem 2rem;
+        margin-right: 40px;
+        text-decoration: none;
+        background-color: #F2A71E ;
+        color: #3276c9;
+        font-family: Arial, sans-serif;
+        font-size: 1rem;
+        text-align: center;
+        transition: background 250ms ease-in-out, 
+        transform 150ms ease;
+    }
+
+
+    button {
+        display: inline-block;
+        border: none;
+        border-radius: 10px;
+        padding: 1rem 2rem;
+        margin: 0;
+        text-decoration: none;
+        background: #3276c9;
+        color: #ffffff;
+        font-family: Arial, sans-serif;
+        font-size: 1rem;
+        cursor: pointer;
+        text-align: center;
+        transition: background 250ms ease-in-out, 
+        transform 150ms ease;
+    }
+
+    button:hover {
+        background: #F2A71E;
+    }
+
+    button:focus {
+        outline: 1px solid #fff;
+        outline-offset: -4px;
+    }
+
+    button:active {
+        transform: scale(0.99);
+    }
+}
+
 </style>
