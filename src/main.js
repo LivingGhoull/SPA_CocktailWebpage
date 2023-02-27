160d1146b5fd0ba95091a3f25a71911393da4ae1
Jesper Kjærgaard <jezper@hotmail.dk>
2023-02-27 14:18:46 +0100

    p

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import router from './router'
 
 import './assets/reset.css'
 
+console.log('all is importet!')
+
 const app = createApp(App)
+console.log('1')
 
 app.use(router)
+console.log('2')
 
 app.mount('#app')
+console.log('Done')
